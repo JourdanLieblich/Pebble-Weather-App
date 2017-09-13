2ecb82a9c98cd72b3e9aacedb9a5dfa473c959f7
Jourdan Lieblich <jslieblich@gmail.com>
2017-09-13 12:03:38 -0700

    Initial Commit

---
+++ src/c/main.c
@@ -0,0 +1,10 @@
+#include <pebble.h>
+#include "src/c/splash_window.h"
+
+
+int main(){
+  splash_window_create();
+  window_stack_push(splash_window_get_window(), true);
+  app_event_loop();
+  splash_window_destroy();
+}
